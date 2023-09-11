0e4d8e7ceb39e7154f58f28e10d1937a94b75d38
Andrew Stellwag <aestellwag@gmail.com>
2023-09-11 10:28:26 -0400

    Added URL validation for assets

--- serverless-functions/src/functions/features/send-to-voice-message/flex/send-to-voice-message.js
+++ serverless-functions/src/functions/features/send-to-voice-message/flex/send-to-voice-message.js
@@ -2,6 +2,7 @@ const { prepareFlexFunction, extractStandardResponse } = require(Runtime.getFunc
   'common/helpers/function-helper'
 ].path);
 const ConferenceOperations = require(Runtime.getFunctions()['common/twilio-wrappers/conference-participant'].path);
+const axios = require('axios');
 
 const requiredParameters = [
   {
@@ -24,9 +25,26 @@ const requiredParameters = [
 exports.handler = prepareFlexFunction(requiredParameters, async (context, event, callback, response, handleError) => {
   try {
     const { conference, agentParticipant, customerParticipant, voiceMessage } = event;
-    // Play the TwiML message to the participant
     const client = context.getTwilioClient();
     const voiceURL = voiceMessage;
+
+    // Since we are testing asset audio paths it will return 200 if the asset is public or 403 if it's private.  Let's also treat 403s as a success
+    const modifiedAxios = axios.create({
+      validateStatus(status) {
+        return status === 200 || status === 403;
+      },
+    });
+
+    // Let's ensure the URL provide is valid, if it isn't, we return the error and keep the agent connects
+    const urlResponse = await modifiedAxios.get(voiceURL);
+    if (urlResponse.status === 403 || urlResponse.status === 200) {
+      console.log('URL is valid');
+    } else {
+      console.log('URL is not valid');
+      return handleError(
+        `The asset ${voiceURL} - is returning a ${urlResponse}, please check you have the prompts.json asset configured properly`,
+      );
+    }
     await client.conferences(conference).participants(customerParticipant).update({
       announceUrl: voiceURL,
     });
@@ -47,6 +65,8 @@ exports.handler = prepareFlexFunction(requiredParameters, async (context, event,
     });
     return callback(null, response);
   } catch (error) {
-    return handleError(error);
+    return handleError(
+      `The asset ${voiceURL} - is returning a ${error}, please check you have the prompts.json asset configured properly`,
+    );
   }
 });
